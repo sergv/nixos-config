4bdf31ff8ae08f3ff9706d7b92c33d5b5952198d
Sergey Vinokurov <serg.foo@gmail.com>
2022-08-03 12:36:09 +0100

    Add script for resetting local usb-to-ethernent device and network bridges dependent on it after pm-suspend

--- scripts/default.nix
+++ scripts/default.nix
@@ -9,16 +9,18 @@ let writePatchedScriptBin = name: buildInputs:
         buildInputs = [ pkgs.makeWrapper ];
         postBuild   = "wrapProgram \"$out/bin/${name}\" --prefix PATH : \"$out/bin\"";
       };
+    reset-usb = writePatchedScriptBin "reset-usb" [];
 in {
-  reset-usb  = writePatchedScriptBin "reset-usb"  [];
-  tar7z      = writePatchedScriptBin "tar7z"      [ pkgs.gnutar pkgs.p7zip ];
-  tar7zUltra = writePatchedScriptBin "tar7zUltra" [ pkgs.gnutar pkgs.p7zip ];
-  targz      = writePatchedScriptBin "targz"      [ pkgs.gnutar pkgs.gzip ];
-  targz-fast = writePatchedScriptBin "targz-fast" [ pkgs.gnutar pkgs.gzip ];
-  tarbz2     = writePatchedScriptBin "tarbz2"     [ pkgs.gnutar pkgs.bzip2 ];
-  tarlz      = writePatchedScriptBin "tarlz"      [ pkgs.gnutar pkgs.lzip ];
-  tartar     = writePatchedScriptBin "tartar"     [ pkgs.gnutar ];
-  tarxz      = writePatchedScriptBin "tarxz"      [ pkgs.gnutar pkgs.xz ];
-  untar      = writePatchedScriptBin "untar"      [ pkgs.gnutar pkgs.p7zip pkgs.gzip pkgs.bzip2 pkgs.lzip pkgs.xz ];
-  wm-sh      = writePatchedScriptBin "wm.sh"      [ pkgs.wmctrl pkgs.coreutils ];
+  inherit reset-usb;
+  reset-nixos-usb-network = writePatchedScriptBin "reset-nixos-usb-network" [ reset-usb ];
+  tar7z                   = writePatchedScriptBin "tar7z"                   [ pkgs.gnutar pkgs.p7zip ];
+  tar7zUltra              = writePatchedScriptBin "tar7zUltra"              [ pkgs.gnutar pkgs.p7zip ];
+  targz                   = writePatchedScriptBin "targz"                   [ pkgs.gnutar pkgs.gzip ];
+  targz-fast              = writePatchedScriptBin "targz-fast"              [ pkgs.gnutar pkgs.gzip ];
+  tarbz2                  = writePatchedScriptBin "tarbz2"                  [ pkgs.gnutar pkgs.bzip2 ];
+  tarlz                   = writePatchedScriptBin "tarlz"                   [ pkgs.gnutar pkgs.lzip ];
+  tartar                  = writePatchedScriptBin "tartar"                  [ pkgs.gnutar ];
+  tarxz                   = writePatchedScriptBin "tarxz"                   [ pkgs.gnutar pkgs.xz ];
+  untar                   = writePatchedScriptBin "untar"                   [ pkgs.gnutar pkgs.p7zip pkgs.gzip pkgs.bzip2 pkgs.lzip pkgs.xz ];
+  wm-sh                   = writePatchedScriptBin "wm.sh"                   [ pkgs.wmctrl pkgs.coreutils ];
 }
