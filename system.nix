6be66b02b771e58dd04feb866f7f6b76ca4335e4
Sergey Vinokurov <serg.foo@gmail.com>
2022-08-06 11:29:31 +0100

    Enable tor daemon

--- system.nix
+++ system.nix
@@ -640,6 +640,11 @@
     passwordAuthentication = false;
   };
 
+  services.tor = {
+    enable        = true;
+    client.enable = true;
+  };
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
