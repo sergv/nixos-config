a911ae52941aa905b373f5ae20730e8c98129446
Sergey Vinokurov <serg.foo@gmail.com>
2023-04-26 22:23:24 +0100

    Better network interface names

--- system.nix
+++ system.nix
@@ -349,17 +349,20 @@ in
     # networkmanager.enable = true;
     #wireless.enable       = true;  # Enables wireless support via wpa_supplicant.
 
+    # Prefer eth0 to eno1 and the like.
+    usePredictableInterfaceNames = true;
+
     # Don’t autoconfigure all network interfaces
     useDHCP = false;
     bridges = {
       br0 = {
-        interfaces = ["eth-usb" "eno1"];
+        interfaces = ["eth-usb" "eth0"];
       };
     };
     interfaces.br0 = {
       useDHCP = true;
     };
-    #interfaces.eno1 = {
+    #interfaces.eth0 = {
     #  useDHCP = true;
     #};
   };
