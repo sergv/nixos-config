532eb7134604fe0f212ff552243d52d96f78da9b
Sergey Vinokurov <serg.foo@gmail.com>
2024-03-10 21:17:02 +0000

    Add label to differentiate -march builds vs vanilla ones

--- system.nix
+++ system.nix
@@ -702,6 +702,8 @@ in
     memoryPercent = 33;
   };
 
+  system.nixos.label = "arch-generic";
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
