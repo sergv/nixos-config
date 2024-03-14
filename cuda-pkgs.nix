01b80f8837010e2e3f1028857f2c0f59003619d4
Sergey Vinokurov <serg.foo@gmail.com>
2024-03-14 22:58:58 +0000

    Introduce wrapper for nvcc that knows where to find CUDA headers and properly sets RPATH of the produced executable so that it will be able to locate libcuda.so at runtime (NB this reference is not shown in ldd! it happens somewhere within the CUDA framework)

--- cuda-pkgs.nix
+++ cuda-pkgs.nix
@@ -1,6 +1,55 @@
 { pkgs
 }:
-{
-  cuda        = pkgs.cudaPackages.cudatoolkit;
+let cuda   = pkgs.cudaPackages.cudatoolkit;
+    cudart = pkgs.cudaPackages.cuda_cudart;
+
+    # Need to patch produced executable in the end so that it will look up for libcuda.so
+    # in /run/opengl-driver/lib directory.
+    wrapped-nvcc = pkgs.writeScriptBin "nvcc"
+      ''
+        #!${pkgs.bash}/bin/bash
+        set -eu
+
+        target="a.out"
+        target_is_next="0"
+
+        for x in "''${@}"; do
+          if [[ "$target_is_next" = 1 ]]; then
+            target="$x"
+            target_is_next="0"
+          elif [[ "$x" = "-o" ]]; then
+            target_is_next="1"
+          elif [[ "$x" = "--output-file" ]]; then
+            target_is_next="1"
+          elif [[ "$x" = -o* ]]; then
+            target="''${x#-o}"
+          elif [[ "$x" = --output-file=* ]]; then
+            target="''${x#--output-file=}"
+          fi
+        done
+
+        "${cuda}/bin/nvcc" --compiler-bindir "${pkgs.gcc12}/bin/gcc" --system-include "${cudart}/include" --library-path "${cudart}/lib" "''${@}"
+
+        if [[ ! -z "$target" && -f "$target" ]]; then
+          ${pkgs.patchelf}/bin/patchelf --add-rpath /run/opengl-driver/lib "$target"
+        fi
+    '';
+
+    wrapped-cuda =
+      pkgs.runCommand ("wrapped-cuda-" + cuda.version) {
+        build-inputs = [ wrapped-nvcc cudart ];
+      }
+        ''
+          mkdir -p "$out/bin"
+          ln -s "${wrapped-nvcc}/bin/nvcc" "$out/bin/"
+          for x in ${cuda}/bin/*; do
+            if [[ "$x" != */nvcc && -f "$x" && -x "$x" ]]; then
+              ln -s "$x" "$out/bin/"
+            fi
+          done
+        '';
+
+in {
+  cuda = wrapped-cuda;
   # cuda-opencl = pkgs.cudaPackages.cuda_opencl;
 }
