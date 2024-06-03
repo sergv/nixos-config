ab1222fee16b675749b4d7c9b78da0c029432080
Sergey Vinokurov <serg.foo@gmail.com>
2024-06-03 12:00:51 +0100

    Fix typo in linked paths for FontForge config

--- system.nix
+++ system.nix
@@ -193,7 +193,7 @@ in
         ".config/bitcoin"
         ".config/chromium"
         ".config/dconf"
-        ".config/fontforce"
+        ".config/fontforge"
         ".config/Google"
         ".config/htop"
         ".config/keybase"
