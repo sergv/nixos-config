64b4747c6056fdb89c6096d4068b7d1536a05baf
Sergey Vinokurov <serg.foo@gmail.com>
2024-10-05 15:23:34 +0100

    Tor relay doesn’t work with my current Internet Service Provider - cannot forward ports

--- system.nix
+++ system.nix
@@ -745,7 +745,7 @@ in
 
     # Enable and configure the Tor relay
     relay = {
-      enable = true;
+      enable = false;
       role = "relay";  # Set the relay role (e.g., "relay", "bridge")
     };
 
