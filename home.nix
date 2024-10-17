74f4977570af8437936cacb24440f68b134af754
Sergey Vinokurov <serg.foo@gmail.com>
2024-10-17 21:16:00 +0100

    Regroup remapping files a bit

--- home.nix
+++ home.nix
@@ -460,11 +460,17 @@ in
         ".vimrc"
 
         ".config/Audaciousrc"
+        ".config/PCSX2"
         ".config/QtProject.conf"
         ".config/Triblerrc"
 
         ".local/ghci.conf"
+        ".local/share/krunnerstaterc"
+        ".local/share/qBittorrent"
         ".local/share/recently-used.xbel"
+        ".local/share/user-places.xbel"
+        ".local/share/user-places.xbel.bak"
+        ".local/share/user-places.xbel.tbcache"
 
         "github-recovery-codes.txt"
         #"mars.exe"
@@ -545,14 +551,6 @@ in
         ".config/Trolltech.conf"
         ".config/user-dirs.dirs"
         ".config/user-dirs.locale"
-
-        ".config/PCSX2"
-
-        ".local/share/krunnerstaterc"
-        ".local/share/qBittorrent"
-        ".local/share/user-places.xbel"
-        ".local/share/user-places.xbel.bak"
-        ".local/share/user-places.xbel.tbcache"
     ];
 
   dconf.settings = {
