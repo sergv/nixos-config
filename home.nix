0f7784f57ae90ba8c2c179bab707263b780fddf1
Sergey Vinokurov <serg.foo@gmail.com>
2025-01-07 12:45:10 +0000

    Use fahclient from upstream packages

--- home.nix
+++ home.nix
@@ -147,25 +147,6 @@ let
       python3 = tribler-python;
     });
 
-  fahclient-pkg = import ./fahclient.nix {
-    inherit (pkgs)
-      lib
-      buildFHSEnv
-      fetchFromGitHub
-      ocl-icd
-      openssl
-      scons
-      stdenv
-      re2
-      libevent
-      git
-      ;
-    extraPkgs = [
-      pkgs.expat
-      pkgs.zlib
-    ];
-  };
-
   isabelle-pkg = import ./isabelle/isabelle.nix {
     inherit pkgs;
   };
@@ -819,6 +800,7 @@ in
       pkgs.cpu-x
       pkgs.curl
       pkgs.dmidecode
+      pkgs.fahclient
       pkgs.ffmpeg-full
       pkgs.file
       pkgs.findutils
@@ -866,8 +848,6 @@ in
       # Music
       pkgs.audacious
       pkgs.strawberry
-
-      fahclient-pkg
 
       pkgs.i2p
       pkgs.xd
