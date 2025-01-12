5c2109c2101742bfba058fcea2a41715131c8a77
Sergey Vinokurov <serg.foo@gmail.com>
2025-01-12 22:38:19 +0000

    Better update scripts since WSL doesn't have boot loader and different system generations

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -o pipefail
 export NIX_BUILD_CORES="10"
 export NINJAFLAGS="-j10 -l10"
 
-nix --extra-experimental-features nix-command --extra-experimental-features flakes build .#nixosConfigurations."work-wsl".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --verbose --max-jobs 2 --cores 10 --keep-going "${@}"
+nix --extra-experimental-features nix-command --extra-experimental-features flakes build .#nixosConfigurations."work-wsl".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --verbose --max-jobs 4 --cores 10 --keep-going "${@}"
 
 # ssh-agent
 # ssh-add /home/sergey/.ssh/nix-cache-ro.key
