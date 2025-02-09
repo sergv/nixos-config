7608d31083866b92f45327a8057d2801680112d0
Sergey Vinokurov <serg.foo@gmail.com>
2025-02-09 10:18:36 +0000

    Set up GHC cross-compiler to Windows (aka ‘x86_64-w64-mingw32’) target

--- flake.nix
+++ flake.nix
@@ -78,6 +78,12 @@
       url = "github:nix-community/NUR";
     };
 
+    haskellNix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
+    };
+
   };
 
   outputs =
@@ -93,6 +99,7 @@
       impermanence,
       arkenfox,
       nur,
+      haskellNix,
       ...
     }:
     let
@@ -361,6 +368,13 @@
         );
       };
 
+      use-win32-thread-model-overlay = final: old: {
+        threadsCross = {
+          model = "win32";
+          package = null;
+        };
+      };
+
       # git-proxy = "http://LOGIN:PASSWORD@HOST:PORT";
       #
       # git-proxy-conf = {
@@ -401,6 +415,16 @@
           fcitx-overlay
           ssh-overlay
           improve-fetchgit-overlay
+        ];
+      };
+
+      pkgs-cross-win = import nixpkgs-unstable {
+        inherit system;
+        inherit (haskellNix) config;
+        overlays = [
+          haskellNix.overlay
+          improve-fetchgit-overlay
+          # use-win32-thread-model-overlay
         ];
       };
 
@@ -463,6 +487,7 @@
           system
           arkenfox
           ;
+        inherit pkgs-cross-win;
         inherit git-proxy-conf;
         pinned-pkgs = {
           nixpkgs-18-09 = import nixpkgs-18-09 { inherit system; };
