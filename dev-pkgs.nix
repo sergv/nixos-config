54b1cb0dec9ee382dbb2c547289b3c0691b9fa98
Sergey Vinokurov <serg.foo@gmail.com>
2025-02-22 16:06:19 +0000

    Make sure ‘x86_64-w64-mingw32-windres’ is available to ‘ghc-win’

--- dev-pkgs.nix
+++ dev-pkgs.nix
@@ -694,6 +694,8 @@ let #pkgs-pristine = nixpkgs-unstable.legacyPackages."${system}";
                     pkg
                     # So that ghc and its subcommands will be able to run ‘x86_64-w64-mingw32-gcc’
                     win-pkgs.buildPackages.gcc.cc
+                    # For ‘x86_64-w64-mingw32-windres’
+                    win-pkgs.buildPackages.binutils
                   ];
                   # "-L${win-pkgs.libffi}/bin" \
                   # "-L${win-pkgs.libffi}/lib" \
