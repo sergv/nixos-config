e148be61a364e5a8fb7901527d15e6e95b3bfe3a
Sergey Vinokurov <serg.foo@gmail.com>
2025-09-13 00:39:26 +0100

    Disable ‘amdgpu’ module so that AMD integrated graphics will not cause troubles

--- system.nix
+++ system.nix
@@ -26,6 +26,11 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
+  boot.blacklistedKernelModules = [
+    # Don’t want my integrated GPU around at all.
+    "amdgpu"
+  ];
+
   boot.kernelParams = [
     "mitigations=off"
     "preempt=full"
