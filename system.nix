e114247e929bc46e1f9b1fd92e7a4a1232d23bb9
Sergey Vinokurov <serg.foo@gmail.com>
2025-12-07 12:10:14 +0000

    Add note for potential future Wayland adoption

--- system.nix
+++ system.nix
@@ -669,6 +669,7 @@ in
     # longitude   = "30.5233";
   };
 
+  # For Wayland support use https://gitlab.com/chinstrap/gammastep
   services.redshift = {
     enable = true;
     executable = "/bin/redshift";
