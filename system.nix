1637ddd38b9d620d93c00f79b838c3ca9365f5e9
Sergey Vinokurov <serg.foo@gmail.com>
2025-12-14 11:31:10 +0000

    Deproritise nix-daemon so that heavy builds leave system responsive

--- system.nix
+++ system.nix
@@ -551,6 +551,9 @@ in
     # extraOptions = pkgs.lib.optionalString (config.nix.package == pkgs.nixVersions.stable)
     #   "experimental-features = nix-command flakes";
 
+    daemonCPUSchedPolicy = "idle";
+    daemonIOSchedClass = "idle";
+
     sshServe = {
       enable = true;
       keys = [ "TODO: add public key here" ];
