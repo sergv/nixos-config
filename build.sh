7cf2678d6fe045cab0741886b059904f80ae132f
Sergey Vinokurov <serg.foo@gmail.com>
2025-12-14 11:10:41 +0000

    Add notes for using trusted substituters via ssh

--- build.sh
+++ build.sh
@@ -13,6 +13,10 @@ set -o pipefail
 export NIX_BUILD_CORES="16"
 export NINJAFLAGS="-j16 -l16"
 
+# ssh-agent
+# ssh-add /home/sergey/.ssh/nix-cache-ro.key
+# --option extra-substituters ssh://nix-ssh@192.168.1.226?trusted=true
+
 nix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --verbose -j2 --cores 16 --keep-going "${@}"
 
 # exec ./apply-system.sh build "${@}"
