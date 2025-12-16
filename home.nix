e395a4d20b6e01de2dd8c7112d01c53440faaab4
Sergey Vinokurov <serg.foo@gmail.com>
2025-12-16 23:58:09 +0000

    Generate reasonable fontconfig with antialiasing and hinting

--- home.nix
+++ home.nix
@@ -340,7 +340,11 @@ in
     };
   };
 
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable       = true;
+    hinting      = "full";
+    antialiasing = true;
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
