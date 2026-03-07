d9e5f79518c643ed0cafc2eaf2e2bee0ffd834af
Sergey Vinokurov <serg.foo@gmail.com>
2026-03-07 16:59:24 +0000

    Setup for WSL

--- build-system-debug.sh
+++ build-system-debug.sh
@@ -10,10 +10,10 @@ set -u
 # propagate errors from all parts of pipes
 set -o pipefail
 
-export NIX_BUILD_CORES="16"
-export NINJAFLAGS="-j16 -l16"
+export NIX_BUILD_CORES="10"
+export NINJAFLAGS="-j10 -l10"
 
-nix build --no-eval-cache .#nixosConfigurations."home".config.system.build.toplevel --show-trace --refresh --out-link /tmp/nixos-rebuild-result/result --verbose -j2 --cores 16 --keep-going "${@}"
+nix build --no-eval-cache .#nixosConfigurations."work-wsl".config.system.build.toplevel --show-trace --refresh --out-link /tmp/nixos-rebuild-result/result --verbose -j2 --cores 16 --keep-going "${@}"
 
 
 exit 0
