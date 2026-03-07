c8c03a6aa137a8e3717a2f80a6070caa5778078f
Sergey Vinokurov <serg.foo@gmail.com>
2026-03-07 16:49:31 +0000

    Lighter config for notebook

--- build.sh
+++ build.sh
@@ -10,16 +10,15 @@ set -u
 # propagate errors from all parts of pipes
 set -o pipefail
 
-export NIX_BUILD_CORES="16"
-export NINJAFLAGS="-j16 -l16"
+export NIX_BUILD_CORES="1"
+export NINJAFLAGS="-j1 -l1"
 
 # ssh-agent
 # ssh-add /home/sergey/.ssh/nix-cache-ro.key
 # --option extra-substituters ssh://nix-ssh@192.168.1.226?trusted=true
 
-# nix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --verbose -j2 --cores 16 --keep-going "${@}"
-trix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result -j2 --cores 16 --keep-going "${@}" "${@}"
-
+# nix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --verbose --max-jobs 1 --cores 2 --keep-going "${@}"
+trix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --max-jobs 1 --cores 2 "${@}"
 
 # exec ./apply-system.sh build "${@}"
 
