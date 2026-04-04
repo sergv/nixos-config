a1d4884644d33fa2b417add5811da3e10242b607
Sergey Vinokurov <serg.foo@gmail.com>
2026-04-04 19:14:25 +0100

    Bump kernel version to 6.18.21

--- kernel.nix
+++ kernel.nix
@@ -1,13 +1,25 @@
 { bore-scheduler-src, kernel-march-patches, linuk-tkg-src }:
 
 { self, lib, pkgs, ... }: {
-  boot =
-    let kernelPkgs = pkgs.linuxKernel.packages.linux_6_17;
 
-        patches = import ./kernel-patches.nix {
-          inherit lib bore-scheduler-src kernel-march-patches linuk-tkg-src;
-          kernelVersion = lib.versions.majorMinor kernelPkgs.kernel.version;
-        };
+  boot.kernelPackages =
+    let kernel = pkgs.linuxKernel.kernels.linux_6_18.override (_: {
+          argsOverride = {
+            version       = "6.18.21";
+            modDirVersion = "6.18.21";
+            src           = pkgs.fetchurl {
+              # url    = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.21.tar.xz";
+              url    = "mirror://kernel/linux/kernel/v6.18.x/linux-6.18.21.tar.xz";
+              sha256 = "sha256-HDghT7E3uuhbgrglN7WYc1hiG5Fasqjk8J5gaXwZR08=";
+            };
+          };
+        });
+
+        patches = # builtins.abort kernel.version;
+          import ./kernel-patches.nix {
+            inherit lib bore-scheduler-src kernel-march-patches linuk-tkg-src;
+            kernelVersion = lib.versions.majorMinor kernel.version;
+          };
 
         # Inspired by
         # https://github.com/lovesegfault/nix-config/blob/db3262d344a10ed589539f834a15e9988f8dba0e/nix/overlays/linux-lto.nix
@@ -51,22 +63,23 @@
         llvmStdenv = stdenvPlatformLLVM;
 
         llvm = kernel:
-          kernel.override {
-            stdenv                             = llvmStdenv;
-            buildPackages                      = pkgs.buildPackages // { stdenv = llvmStdenv; };
-            argsOverride.kernelPatches         = kernel.kernelPatches;
-            argsOverride.structuredExtraConfig = kernel.structuredExtraConfig;
-          };
+          kernel.override (old: {
+            stdenv        = llvmStdenv;
+            buildPackages = pkgs.buildPackages // { stdenv = llvmStdenv; };
+          });
 
-    in {
-      kernelPackages = (pkgs.linuxKernel.packagesFor ((llvm kernelPkgs.kernel).override (old: {
-        argsOverride.kernelPatches = old.kernelPatches ++ patches;
-      }))).extend (final: prev: {
-        virtualbox = prev.virtualbox.overrideAttrs (final2: prev2: {
-          # Export environment variable for kernel’s module compilation makefile.
-          # It’s there where decision to pick clang vs gcc is taken, not in virtualbox’s makefile.
-          "LLVM" = "1";
-        });
-      });
-    };
+        finalPackages =
+          (pkgs.linuxKernel.packagesFor ((llvm kernel).override (old: {
+            argsOverride = old.argsOverride // {
+              kernelPatches = old.kernelPatches ++ patches;
+            };
+          }))).extend (final: prev: {
+            virtualbox = prev.virtualbox.overrideAttrs (final2: prev2: {
+              # Export environment variable for kernel’s module compilation makefile.
+              # It’s there where decision to pick clang vs gcc is taken, not in virtualbox’s makefile.
+              "LLVM" = "1";
+            });
+          });
+
+    in finalPackages;
 }
