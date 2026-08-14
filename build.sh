da740b695ff78cb9993077afc91478dd4ee99d42
Sergey Vinokurov <serg.foo@gmail.com>
2026-08-15 00:33:30 +0100

    Generalize build.sh to be able to build multiple configurations at once

--- build.sh
+++ build.sh
@@ -17,7 +17,17 @@ export NINJAFLAGS="-j16 -l16"
 # ssh-add /home/sergey/.ssh/nix-cache-ro.key
 # --option extra-substituters ssh://nix-ssh@192.168.1.226?trusted=true
 
-nix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --verbose -j4 --cores 16 --keep-going "${@}"
+# nix build .#nixosConfigurations."wsl".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result --verbose -j4 --cores 16 --keep-going "${@}"
+
+declare -a targets
+
+for target in "${@}"; do
+    targets+=(".#nixosConfigurations.\"$target\".config.system.build.toplevel")
+done
+
+nix build --out-link /tmp/nixos-rebuild-result/result --verbose -j4 --cores 16 --keep-going "${targets}"
+
+
 # trix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result -j2 --cores 16 --keep-going "${@}" "${@}"
 
 
