362286bc8c9adc2950dc4497325c981f8fbbe879
Sergey Vinokurov <serg.foo@gmail.com>
2026-08-22 18:04:41 +0100

    Improve build.sh to not use more cores that CPUs

--- build.sh
+++ build.sh
@@ -43,7 +43,24 @@ if [[ "${#targets[@]}" == 0 ]]; then
     exit 1
 fi
 
-nix build --out-link /tmp/nixos-rebuild-result/result --verbose -j4 --cores 16 --keep-going "${targets[@]}" "${opts[@]}"
+cores="1"
+if [[ -v NIX_BUILD_CORES ]]; then
+    cores="$NIX_BUILD_CORES"
+else
+    cores="$(getconf _NPROCESSORS_ONLN)"
+    if [[ "$OSTYPE" == "linux-gnu" ]] && command -v lscpu >/dev/null 2>&1; then
+        threads_per_core=$(lscpu | awk '/^ *Thread\(s\) per core:/ { print $NF; }')
+        cores=$(( "$cores" / "$threads_per_core" ))
+        # cores=$(lscpu | awk 'BEGIN { cores = 0; threads = 0; } /^ *CPU\(s\):/ { cores = $NF; } /^ *Thread\(s\) per core:/ { threads = $NF; } END { print (cores / threads); }')
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        cores="$cores"
+        # cores=$(sysctl machdep.cpu.core_count | cut -w -f2)
+    elif [[ -e /proc/cpuinfo ]]; then
+        cores="$(awk '/processor/' /proc/cpuinfo | wc -l)"
+    fi
+fi
+
+nix build --out-link /tmp/nixos-rebuild-result/result --verbose -j4 --cores "$cores" --keep-going "${targets[@]}" "${opts[@]}"
 
 
 # trix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result -j2 --cores 16 --keep-going "${@}" "${@}"
