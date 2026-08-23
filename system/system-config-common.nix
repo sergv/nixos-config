4db7e1e038bda26258d584ed77a016ca7ca7a331
Sergey Vinokurov <serg.foo@gmail.com>
2026-08-23 14:28:04 +0100

    Move linux-only packages into system/system-config-linux-common

--- system/system-config-common.nix
+++ system/system-config-common.nix
@@ -12,39 +12,11 @@
   # $ nix-env -qaP | grep wget
   environment = {
     systemPackages = [
-      pkgs.alsa-tools
-      pkgs.alsa-utils
-      pkgs.killall
-      # pkgs.libnotify # for showing notifications in wm_operate.py
-      # pkgs.libreoffice
-      pkgs.perf
-      pkgs.ltrace
       pkgs.man
       pkgs.man-pages
-      pkgs.mkpasswd
-      pkgs.pciutils
-      pkgs.sudo
-      pkgs.strace
       pkgs.trix
       pkgs.vim
-      # pkgs.veracrypt
-      #(pkgs.wineFull.override { netapiSupport = false; })
-
-      # pkgs.bumblebee
-      # pkgs.jdk7
-      # pkgs.jdk
-      # pkgs.ocaml
-      # pkgs.octaveFull
-      # pkgs.python36Packages.ipython
-      # pkgs.python36Packages.jupyter
-      # pkgs.python36Packages.jupyter_client
-      # pkgs.python36Packages.matplotlib
-      # pkgs.python36Packages.sympy
-
-      #nix-bash-completions
-
-      # # For Xfce
-      # pkgs.networkmanagerapplet
+      # pkgs.nix-bash-completions
     ];
   };
 
--- system/system-config-linux-common.nix
+++ system/system-config-linux-common.nix
@@ -1,5 +1,41 @@
 { config, pkgs, ... }:
 {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment = {
+    systemPackages = [
+      pkgs.alsa-tools
+      pkgs.alsa-utils
+      pkgs.killall
+      # pkgs.libnotify # for showing notifications in wm_operate.py
+      # pkgs.libreoffice
+      pkgs.perf
+      pkgs.ltrace
+      pkgs.mkpasswd
+      pkgs.pciutils
+      pkgs.sudo
+      pkgs.strace
+      # pkgs.veracrypt
+      #(pkgs.wineFull.override { netapiSupport = false; })
+
+      # pkgs.bumblebee
+      # pkgs.jdk7
+      # pkgs.jdk
+      # pkgs.ocaml
+      # pkgs.octaveFull
+      # pkgs.python36Packages.ipython
+      # pkgs.python36Packages.jupyter
+      # pkgs.python36Packages.jupyter_client
+      # pkgs.python36Packages.matplotlib
+      # pkgs.python36Packages.sympy
+
+      #nix-bash-completions
+
+      # # For Xfce
+      # pkgs.networkmanagerapplet
+    ];
+  };
+
   networking = {
     firewall = {
       enable = true;
