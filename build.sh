5118ea37dea98da3b2601a6ec0e70b44eee91294
Sergey Vinokurov <serg.foo@gmail.com>
2026-08-29 21:43:15 +0100

    Improve build scripts to not produce too much jobs on MacOS

--- build.sh
+++ build.sh
@@ -10,9 +10,6 @@ set -u
 # propagate errors from all parts of pipes
 set -o pipefail
 
-export NIX_BUILD_CORES="16"
-export NINJAFLAGS="-j16 -l16"
-
 # ssh-agent
 # ssh-add /home/sergey/.ssh/nix-cache-ro.key
 # --option extra-substituters ssh://nix-ssh@192.168.1.226?trusted=true
@@ -44,6 +41,7 @@ if [[ "${#targets[@]}" == 0 ]]; then
 fi
 
 cores="1"
+jobs="4"
 if [[ -v NIX_BUILD_CORES ]]; then
     cores="$NIX_BUILD_CORES"
 else
@@ -53,14 +51,18 @@ else
         cores=$(( "$cores" / "$threads_per_core" ))
         # cores=$(lscpu | awk 'BEGIN { cores = 0; threads = 0; } /^ *CPU\(s\):/ { cores = $NF; } /^ *Thread\(s\) per core:/ { threads = $NF; } END { print (cores / threads); }')
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-        cores="$cores"
+        cores="$(( cores - 2 ))"
+        jobs="2"
         # cores=$(sysctl machdep.cpu.core_count | cut -w -f2)
     elif [[ -e /proc/cpuinfo ]]; then
         cores="$(awk '/processor/' /proc/cpuinfo | wc -l)"
     fi
 fi
 
-nix build --out-link /tmp/nixos-rebuild-result/result --verbose -j4 --cores "$cores" --keep-going "${targets[@]}" "${opts[@]}"
+export NIX_BUILD_CORES="$cores"
+export NINJAFLAGS="-j$cores -l$cores"
+
+nix build --out-link /tmp/nixos-rebuild-result/result --verbose -j "$jobs" --cores "$cores" --keep-going "${targets[@]}" "${opts[@]}"
 
 
 # trix build .#nixosConfigurations."home".config.system.build.toplevel --out-link /tmp/nixos-rebuild-result/result -j2 --cores 16 --keep-going "${@}" "${@}"
